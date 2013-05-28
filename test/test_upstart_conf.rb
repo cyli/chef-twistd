8d4a9c76994a59dce492a840ce3597e41cdd502c
cyli <cyli@twistedmatrix.com>
2013-05-27 22:05:39 -0700

    Add the rest of the test cases for the template and add whitespace fixes.  Fixes #3

--- test/test_upstart_conf.rb
+++ test/test_upstart_conf.rb
@@ -35,9 +35,8 @@ class TestTemplate < Minitest::Test
       respawn
 
       exec twistd -n \\
-          --pidfile /tmp/pidfile
+          --pidfile /tmp/pidfile \\
           command
-
     eof
     assert_equal expected, result
   end
@@ -62,10 +61,151 @@ class TestTemplate < Minitest::Test
       respawn
 
       exec su - me -c 'twistd -n \\
-          --pidfile /tmp/pidfile
+          --pidfile /tmp/pidfile \\
           command'
-
     eof
     assert_equal expected, result
   end
+
+  # If the authbind ports are provided but the user is root, not authbinded
+  def test_authbind_user_root
+    result = TEMPLATE.evaluate(
+      :service_name => 'service',
+      :user => 'root',
+      :twistd_command => 'command',
+      :pidfile => '/tmp/pidfile',
+      :args => [],
+      :authbinded => true
+    )
+
+    expected = <<-eof.gsub(/^ {6}/, '')
+      description                     "Twistd service: service"
+      author                          "Ying Li <cyli@twistedmatrix.com>"
+
+      start on runlevel [2345]
+      stop on runlevel [016]
+
+      respawn
+
+      exec twistd -n \\
+          --pidfile /tmp/pidfile \\
+          command
+    eof
+    assert_equal expected, result
+  end
+
+  # If the authbind ports are provided and the user is not root, authbinded
+  def test_authbind_user_not_root
+    result = TEMPLATE.evaluate(
+      :service_name => 'service',
+      :user => 'me',
+      :twistd_command => 'command',
+      :pidfile => '/tmp/pidfile',
+      :args => [],
+      :authbinded => true
+    )
+
+    expected = <<-eof.gsub(/^ {6}/, '')
+      description                     "Twistd service: service"
+      author                          "Ying Li <cyli@twistedmatrix.com>"
+
+      start on runlevel [2345]
+      stop on runlevel [016]
+
+      respawn
+
+      exec su - me -c 'authbind --deep twistd -n \\
+          --pidfile /tmp/pidfile \\
+          command'
+    eof
+    assert_equal expected, result
+  end
+
+  # If the logfile is provided, include it as an option
+  def test_include_logfile
+    result = TEMPLATE.evaluate(
+      :service_name => 'service',
+      :user => 'root',
+      :twistd_command => 'command',
+      :pidfile => '/tmp/pidfile',
+      :args => [],
+      :logfile => '/var/log/log'
+    )
+
+    expected = <<-eof.gsub(/^ {6}/, '')
+      description                     "Twistd service: service"
+      author                          "Ying Li <cyli@twistedmatrix.com>"
+
+      start on runlevel [2345]
+      stop on runlevel [016]
+
+      respawn
+
+      exec twistd -n \\
+          --pidfile /tmp/pidfile \\
+          --logfile /var/log/log \\
+          command
+    eof
+    assert_equal expected, result
+  end
+
+  # If args are included, each is printed on a separate line
+  def test_line_per_arg
+    result = TEMPLATE.evaluate(
+      :service_name => 'service',
+      :user => 'root',
+      :twistd_command => 'command',
+      :pidfile => '/tmp/pidfile',
+      :args => ['arg1', 'arg2', 'arg3']
+    )
+
+    expected = <<-eof.gsub(/^ {6}/, '')
+      description                     "Twistd service: service"
+      author                          "Ying Li <cyli@twistedmatrix.com>"
+
+      start on runlevel [2345]
+      stop on runlevel [016]
+
+      respawn
+
+      exec twistd -n \\
+          --pidfile /tmp/pidfile \\
+          command \\
+          arg1 \\
+          arg2 \\
+          arg3
+    eof
+    assert_equal expected, result
+  end
+
+  # If args are included, each is printed on a separate line.  The closing
+  # single quote is in the right place, if the user is not root
+  def test_line_per_arg_as_non_root
+    result = TEMPLATE.evaluate(
+      :service_name => 'service',
+      :user => 'me',
+      :twistd_command => 'command',
+      :pidfile => '/tmp/pidfile',
+      :args => ['arg1', 'arg2', 'arg3']
+    )
+
+    expected = <<-eof.gsub(/^ {6}/, '')
+      description                     "Twistd service: service"
+      author                          "Ying Li <cyli@twistedmatrix.com>"
+
+      start on runlevel [2345]
+      stop on runlevel [016]
+
+      respawn
+
+      exec su - me -c 'twistd -n \\
+          --pidfile /tmp/pidfile \\
+          command \\
+          arg1 \\
+          arg2 \\
+          arg3'
+    eof
+    assert_equal expected, result
+  end
+
 end
