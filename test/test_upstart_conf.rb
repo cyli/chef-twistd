2e8e60f179b38b8ba41f5fb3b372b5e62e20d7c2
cyli <cyli@twistedmatrix.com>
2013-05-27 21:26:44 -0700

    Yay some whitespace issues fixed

--- test/test_upstart_conf.rb
+++ test/test_upstart_conf.rb
@@ -5,6 +5,9 @@ require 'minitest/autorun'
 require 'erubis'
 require 'ostruct'
 
+# also, check out http://www.kuwata-lab.com/erubis/users-guide.html,
+# specifically http://www.kuwata-lab.com/erubis/users-guide.06.html for docs
+
 template_file = File.expand_path(File.join(
   File.dirname(__FILE__), "../templates/default/twistd_plugin.conf.erb"))
 
@@ -18,22 +21,24 @@ class TestTemplate < Minitest::Test
       :service_name => 'service',
       :user => 'root',
       :twistd_command => 'command',
+      :pidfile => '/tmp/pidfile',
       :args => []
     )
 
-    expected = [
-      'description                     "Twistd service: service"',
-      'author                          "Ying Li <cyli@twistedmatrix.com>"',
-      '',
-      'start on runlevel [2345]',
-      'stop on runlevel [016]',
-      '',
-      'respawn',
-      '',
-      'exec twistd -n \\',
-      '    command',
-    ].join('\n')
+    expected = <<-eof.gsub(/^ {6}/, '')
+      description                     "Twistd service: service"
+      author                          "Ying Li <cyli@twistedmatrix.com>"
 
+      start on runlevel [2345]
+      stop on runlevel [016]
+
+      respawn
+
+      exec twistd -n \\
+          --pidfile /tmp/pidfile
+          command
+
+    eof
     assert_equal expected, result
   end
 end
