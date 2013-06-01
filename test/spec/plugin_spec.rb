263ca0920b881949eed463638a8bccb0bc9780d7
cyli <cyli@twistedmatrix.com>
2013-06-01 00:22:18 -0700

    Just use rspec, not chefspec

--- test/spec/plugin_spec.rb
+++ test/spec/plugin_spec.rb
@@ -1,35 +1,59 @@
 # ChefSpec tests for the plugin LWRP
-# https://coderwall.com/p/afdnyw - tip for how to test code that executes at
-# compile
+# stole a bunch from https://gist.github.com/jtimberman/4370569
 
-require 'chefspec'
 
-describe 'twistd::default' do
-  let(:chef_run) {
-    runner = ChefSpec::ChefRunner.new({
-      :platform => 'ubuntu',
-      :version => '12.04',
-      :step_into => ['twistd_plugin']
-    })
-    runner.converge 'lwrp_test::plugin'
-  }
-  let(:node) { runner.node }
-  let
-  before(:each) do
-    @events = Chef::EventDispatch::Dispatcher.new
-    @run_context = Chef::RunContext.new(runner.node, {}, @events)
-    @new_resource = Chef::Resource::Service.new("upstart")
+require 'rspec'
+
+require 'chef/platform'
+require 'chef/run_context'
+require 'chef/resource'
+require 'chef/provider'
+require 'chef/cookbook/metadata'
+require 'chef/event_dispatch/dispatcher'
+
+md = Chef::Cookbook::Metadata.new
+md.from_file(File.join(File.dirname(__FILE__), %w[.. .. metadata.rb]))
+
+
+Chef::Resource.build_from_file(
+  md.name,
+  File.join(File.dirname(__FILE__), %w[.. .. resources plugin.rb]),
+  @run_context)
+Chef::Provider.build_from_file(
+  md.name,
+  File.join(File.dirname(__FILE__), %w[.. .. providers plugin.rb]),
+  @run_context)
+
+
+describe 'Chef::Provider::TwistdPlugin' do
+  before do
+    @node = Chef::Node.new
+    @node.set["name"] = 'plugin'
+    @node.set["platform"] = 'ubuntu'
+    @node.set["platform_version"] = '9.10'
+
+    @run_context = Chef::RunContext.new(@node, {},
+                                        Chef::EventDispatch::Dispatcher.new)
+
+    @new_resource = Chef::Resource::TwistdPlugin.new('test_plugin')
+    @provider = Chef::Provider::TwistdPlugin.new(@new_resource, @run_context)
   end
 
-  it 'basic root upstart conf for resource root_no_args' do
-    # because those resources are created at compile time:
-    chef_run.resources.find {
-      |r| r.name == 'root_no_args'}.old_run_action(:create)
-    loc = '/etc/init/root_no_args.conf'
-    expect(chef_run).to create_file loc
-    file = chef_run.template(loc)
-    expect(file).to be_owned_by('root', 'root')
+  describe "load_current_resource" do
+    it 'should start off with authbinded as false' do
+      @provider.load_current_resource
+      @provider.instance_variable_get(:@authbinded).should == false
+    end
+
+    it 'should set pidfile to a sane default if not provided' do
+      @provider.load_current_resource
+      @provider.instance_variable_get(:@pidfile).should == "/tmp/test_plugin.twistd.pid"
+    end
+
+    it 'should set pidfile to a sane default if not provided' do
+      @new_resource.instance_variable_set(:@pidfile, "/my/pid/file")
+      @provider.load_current_resource
+      @provider.instance_variable_get(:@pidfile).should == "/my/pid/file"
+    end
   end
-
-
 end
